05b414a5851850ca8fbbfe113d33c64f186feed1
adm_user2001 <yuquilemajoel@gmail.com>
2024-01-04 09:30:24 -0500

    Test unit

---
+++ test_calculadora.py
@@ -0,0 +1,36 @@
+import unittest
+import calculadora
+
+class TestCalculadora(unittest.TestCase):
+
+    def test_suma(self):
+        self.assertEqual(calculadora.suma(3, 5), 8)
+        self.assertEqual(calculadora.suma(-1, 1), 0)
+        self.assertEqual(calculadora.suma(0, 0), 0)
+
+    
+
+    def test_resta(self):
+        self.assertEqual(calculadora.resta(10, 5), 5)
+        self.assertEqual(calculadora.resta(0, 5), 0)
+        self.assertEqual(calculadora.resta(0, 0), 0)
+
+
+
+    
+    def test_multiplicacion(self):
+        self.assertEqual(calculadora.multiplicacion(3, 5), 15)
+        self.assertEqual(calculadora.multiplicacion(0, 5), 0)
+        self.assertEqual(calculadora.multiplicacion(-2, 2), -4)
+
+
+
+    
+    def test_divicion(self):
+        self.assertEqual(calculadora.division(10, 2), 5)
+        self.assertEqual(calculadora.division(7, 3), 7 / 3)
+        with-self.assertRaises(ValueError):
+            calculadora.division(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()     
